32dd4af571ed30290e7a47176c895b6a3e38a1a3
BennoubaXNacef <BennoubaNacefCODING@gmail.com>
2024-03-01 22:43:42 +0100

    01/03.22:43
    backend

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import AppHome from "@/components/AppHome.vue";
 import AppForm from "@/components/AppForm.vue";
-import AppBackendData from "@/components/AppBackendData.vue"; // Add a component to display data from the backend
 
 const routes = [
   {
@@ -17,7 +16,6 @@ const routes = [
   {
     path: "/backend-data",
     name: "BackendData",
-    component: AppBackendData, // Route to a component that fetches and displays data from the backend
   },
   // Add more routes as needed
 ];
